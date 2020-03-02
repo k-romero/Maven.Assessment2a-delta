70f492b399e97cccf8f649a058d6dc443db5687a
k-romero <imkevinromero@gmail.com>
2020-03-02 10:57:04 -0500

    completed portion of address class

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,11 +1,13 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+
     /**
      * @param strings list of strings to add / remove / fetch from
      */
@@ -17,7 +19,6 @@ public class Inventory {
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
     }
 
     /**
